e3932a6b4bfda7bd3d172c8bb79395dcf1a4c846
Mehrdad Javan <mehrdad.javan@adapta.se>
2023-11-08 09:08:59 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/g46emailsenderapi/G46EmailSenderApiApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.g46emailsenderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G46EmailSenderApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(G46EmailSenderApiApplication.class, args);
+  }
+
+}
